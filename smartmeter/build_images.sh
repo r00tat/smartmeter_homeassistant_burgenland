95ce0f064f668c6feba2b6a58678baff1f1c170a
Paul Woelfel <paul@woelfel.at>
2025-01-03 11:32:35 +0100

    Use all to build all architectures and remove zip file step

--- smartmeter/build_images.sh
+++ smartmeter/build_images.sh
@@ -17,13 +17,17 @@ function builder() {
     $@
 }
 
-for ARCH in aarch64 amd64; do
-  echo "Building version ${VERSION} for ${ARCH}"
-  builder --${ARCH}
-  # docker buildx build -t paulwoelfel/smartmeter_homeassistant_burgenland_${ARCH}:$VERSION --build-arg BUILD_FROM=homeassistant/${ARCH}-base-python:latest --platform linux/${ARCH} .
-  # docker tag paulwoelfel/smartmeter_homeassistant_burgenland_${ARCH}:$VERSION paulwoelfel/smartmeter_homeassistant_burgenland_${ARCH}:latest
-  # docker push paulwoelfel/smartmeter_homeassistant_burgenland_${ARCH}:$VERSION
-  # docker push paulwoelfel/smartmeter_homeassistant_burgenland_${ARCH}:latest
+# for ARCH in aarch64 amd64; do
+#   echo "Building version ${VERSION} for ${ARCH}"
+#   builder --${ARCH}
+#   # docker buildx build -t paulwoelfel/smartmeter_homeassistant_burgenland_${ARCH}:$VERSION --build-arg BUILD_FROM=homeassistant/${ARCH}-base-python:latest --platform linux/${ARCH} .
+#   # docker tag paulwoelfel/smartmeter_homeassistant_burgenland_${ARCH}:$VERSION paulwoelfel/smartmeter_homeassistant_burgenland_${ARCH}:latest
+#   # docker push paulwoelfel/smartmeter_homeassistant_burgenland_${ARCH}:$VERSION
+#   # docker push paulwoelfel/smartmeter_homeassistant_burgenland_${ARCH}:latest
 
-  echo "completed build for ${ARCH}."
-done
+#   echo "completed build for ${ARCH}."
+# done
+
+echo "building version ${VERSION}"
+builder --all
+echo "build complete."
